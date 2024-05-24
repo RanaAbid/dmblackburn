c9be3c81455029f08bf11a85668eb5a749675fbe
Abid <ranaabid52001@outlook.com>
2024-05-24 22:17:07 +0500

    changes

---
+++ our-staff/index.php
@@ -0,0 +1,237 @@
+<?php include("../includes/header.php"); ?>
+<!-- banner begin -->
+<div class="banner breadcrumb-banner pt-190 pb-200">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="banner-txt">
+                    <h1 class="breadcrumb-title">Our Staff</h1>
+                    <div class="breadcrumb-txt">
+                        <a href="index.html">Home</a>
+                        <span class="dvdr"><i class="icofont-simple-right"></i></span>
+                        <span>Our Staff</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- banner end -->
+<div class="our-staff pt-120 pb-80">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="section-heading text-center">
+                    <h2 class="section-title mb-0">Meet Our Staffs</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                <div class="card-staff">
+                    <div class="face face1">
+                        <div class="content text-center">
+                            <h3>Waqas Choudhery</h3>
+                            <p class="text-white heading-sub-txt">Director</p>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p class="heading-sub-txt">Level 3 Safeguarding trained</p>
+                            <p class="heading-sub-txt">FGM trained</p>
+                            <p class="heading-sub-txt">Trained in Safer Recruitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("../includes/footer.php"); ?>
